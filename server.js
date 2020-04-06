2cc0bb4be0f1b35ee69a2a257e4a6e1066920e52
FAF <ainesederfederico@gmail.com>
2020-04-06 15:59:22 -0300

    process.env.PORT

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
