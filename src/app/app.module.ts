f0f678cffee679e1ee2440a7da47cb41970e137a
FAF <ainesederfederico@gmail.com>
2020-04-06 17:14:11 -0300

    set undefined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { BoardComponent } from './components/board/board.component';
 
 import { environment } from './../environments/environment';
 
-const config: SocketIoConfig = { url: 'https://bazas.herokuapp.com', options: {} };
+const config: SocketIoConfig = { url: undefined, options: {} };
 
 
 @NgModule({
